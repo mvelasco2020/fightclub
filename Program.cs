358392a74b46fcd1ba4d9529db3027d8a5fa9283
Mike Velasco <mvelasco2020@outlook.com>
2024-02-10 20:41:18 -0600

    database connection, dbcontext

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using fightclub.Data;
 using fightclub.Services.CharacterService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Database
+builder.Services.AddDbContext<DataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -4,31 +4,29 @@ using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 using AutoMapper;
+using fightclub.Data;
 using fightclub.DTO.Character;
 using fightclub.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Validations;
 
 namespace fightclub.Services.CharacterService
 {
     public class CharacterService : ICharacterService
     {
         private readonly IMapper _mapper;
-        public CharacterService(IMapper mapper)
+        private readonly DataContext _context;
+        public CharacterService(IMapper mapper, DataContext context)
         {
+            _context = context;
             _mapper = mapper;
         }
-
-        private static List<Character> characters = new List<Character>(){
-            new Character(),
-            new Character(){
-                Id = 99,
-                Name = "player2"
-            }
-        };
 
 
         public async Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter)
         {
+            var characters = await _context.Characters.ToListAsync();
             characters.Add(_mapper.Map<Character>(newCharacter));
             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
             serviceResponse.Data = characters.Select(c =>
@@ -41,6 +39,8 @@ namespace fightclub.Services.CharacterService
         public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+
+            var characters = await _context.Characters.ToListAsync();
             serviceResponse.Data = characters.Select(c =>
             _mapper.Map<GetCharacterDTO>(c)
             ).ToList();
@@ -49,6 +49,7 @@ namespace fightclub.Services.CharacterService
 
         public async Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id)
         {
+            var characters = await _context.Characters.ToListAsync();
             var character = characters.FirstOrDefault(c => c.Id == id);
 
             if (character is not null)
@@ -62,6 +63,7 @@ namespace fightclub.Services.CharacterService
 
         public async Task<ServiceResponse<GetCharacterDTO>> UpdateCharacter(UpdateCharacterDTO updateCharacter)
         {
+            var characters = await _context.Characters.ToListAsync();
             var serviceResponse = new ServiceResponse<GetCharacterDTO>();
             try
             {
@@ -94,6 +96,7 @@ namespace fightclub.Services.CharacterService
         public async Task<ServiceResponse<List<GetCharacterDTO>>> DeleteCharacter(int id)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+            var characters = await _context.Characters.ToListAsync();
             try
             {
                 var character = characters.First(c => c.Id == id);
@@ -111,10 +114,6 @@ namespace fightclub.Services.CharacterService
                 serviceResponse.Message = ex.Message;
                 return serviceResponse;
             }
-
-
-
-
 
         }
     }
